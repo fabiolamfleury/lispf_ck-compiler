e3d1f1c33659a0d8aaaa93aab5f2e0f00e4b6023
fabiolamfleury <fabiola.mfleury@gmail.com>
2017-11-24 18:44:26 -0200

    Updating lispfuck functions

--- lispf_ck.py
+++ lispf_ck.py
@@ -1,6 +1,8 @@
 import ox
 import click
 import pprint
+import sys
+from sys import stdin
 
 lexer = ox.make_lexer([
     ('NAME', r'[-a-zA-Z]+'),
@@ -14,13 +16,13 @@ lexer = ox.make_lexer([
 
 
 tokens = ['PARENTESE_F','PARENTESE_A','NUMBER','NAME']
+cellptr = 0
+cells = [0]
+codeptr = 0
 
-operator = lambda type_op: ('operator', type_op)
 name = lambda name: ('name', name)
 number = lambda number: ('number', int(number))
 op = lambda op: (op)
-opr = lambda op, num: (op, num)
-
 parser = ox.make_parser([
 	('program : PARENTESE_A expr PARENTESE_F', lambda x,y,z: y),
     ('program : PARENTESE_A PARENTESE_F', lambda x,y: '()'),
@@ -31,26 +33,29 @@ parser = ox.make_parser([
     ('operator : NUMBER', number),
 ], tokens)
 
-cells, codeptr, cellptr = [0], 0, 0
 
 def right():
+    global cellptr
     cellptr += 1
     if cellptr == len(cells):
         cells.append(0)
 
 def left():
+    global cellptr
     if cellptr <= 0:
         cellptr = 0
     else:
         cellptr -= 1
 
 def inc():
+    global cellptr
     if cells[cellptr] < 255:
         cells[cellptr] = cells[cellptr] + 1
     else:
         cells[cellptr] = 0
 
 def dec():
+    global cellptr
     if cells[cellptr] > 0:
         cells[cellptr] = cells[cellptr] - 1
     else:
@@ -59,40 +64,58 @@ def dec():
 def sub(number):
     while number != 0:
         dec()
-        number--
+        number -= 1
+
+def loop(code):
+  temp_bracestack, bracemap = [], {}
+
+  for position, command in enumerate(code):
+    if command == "[": temp_bracestack.append(position)
+    if command == "]":
+      start = temp_bracestack.pop()
+      bracemap[start] = position
+      bracemap[position] = start
+  return bracemap
+
 
 def add(number):
     while number != 0:
         inc()
-        number
+        number -= 1
 
 def _print():
     sys.stdout.write(chr(cells[cellptr]))
 
 def read():
-    cells[cellptr] = ord(getch.getch())
+    global cellptr, cells
+    cells[cellptr] = ord(stdin.read(1))
 
 OP_TO_FUNC = {
     'right': right(),    # > in brainfuck
     'left': left(),     # < in brainfuck
-    'print': lambda x, y: x * y,     # . in brainfuck
-    'read': lambda x, y: x / y,     # , in brainfuck
-    'add': lambda x, y: x,  # > in brainfuck x times
-    'sub': lambda x: x,     # < in brainfuck x times
-    'loop': lambda x: x,         # [] in brainfuck
-    'inc': lambda x: x,         # + in brainfuck
-    'dec': lambda x: x,         # - in brainfuck
-    'def': lambda x: x,
+    'print': _print(),     # . in brainfuck
+    'read': read(),     # , in brainfuck
+    'loop': loop(),         # [] in brainfuck
+    'inc': inc(),         # + in brainfuck
+    'dec': dec(),         # - in brainfuck
+}
+
+_OP_TO_FUNC = {
+    'add': add(number),  # > in brainfuck x times
+    'sub': sub(number),     # < in brainfuck x times
 }
 
 def eval(ast):
     head, *tail = ast
-    if head == 'atom':
+    if isinstance(head, int):
         return tail[0]
-    elif head in {'+', '-', '*', '/'}:
-        func = OP_TO_FUNC[head]
-        x, y = map(eval, tail)
-        return func(x, y)
+    elif head in OP_TO_FUNC:
+        OP_TO_FUNC[head]()
+        return
+    elif head in _OP_TO_FUNC:
+        func = _OP_TO_FUNC[head]
+        x = map(eval, tail)
+        return func(x)
     else:
         raise ValueError('operador invalido: %s' % head)
 
